428515dcdec0880c843e2f866acb8145f59f20c2
Rafael <rlousta13@gmail.com>
2024-01-02 21:14:33 -0600

    HELLO

---
+++ src/data/urls-data.js
@@ -0,0 +1,7 @@
+module.exports = [
+    {
+      href: "http://www.google.com",
+      id: 1,
+    },
+  ];
+  
--- src/uses/uses.router.js
+++ src/uses/uses.router.js
@@ -1,27 +1,12 @@
-const router = require("express").Router();
-const controller = require("./url.controller");
-
-
-
-
+const router = require("express").Router({ mergeParams: true });
+const controller = require("./uses.controller");
 
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-const usesRouter = require("../uses/uses.router");
 
+router.route("/").get(controller.list).all(methodNotAllowed);
 
-
-router.use("/:urlId/uses", controller.urlExists,usesRouter);
-
-
-router.route("/:urlId").get(controller.read).put(controller.update).all(methodNotAllowed);
-
-
-
-router.route("/").post(controller.create).get(controller.list).all(methodNotAllowed);
-
-
-
+router.route("/:useId").get(controller.read).delete(controller.delete).all(methodNotAllowed);
 
 
 
